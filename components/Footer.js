62a7b554bedec020bffee74180b00543c4edaeec
Abiona Michael <amabiona21@gmail.com>
2021-02-01 12:32:18 +0100

    styling the components, using global and local styles

--- components/Footer.js
+++ components/Footer.js
@@ -1,8 +1,8 @@
 const Footer = () => {
   return (
-    <div>
+    <footer>
       Copyright &copy; 2021 Ninja List
-    </div>
+    </footer>
   );
 }
  
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import Link from 'next/link'
+import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
@@ -8,13 +9,11 @@ export default function Home() {
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <h1>Home Page</h1>
-      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Esse explicabo rem doloremque, fugiat ad, magnam deserunt error commodi mollitia consequuntur hic laboriosam. Velit, dignissimos nulla eaque quis odio magni minus.</p>
-      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Esse explicabo rem doloremque, fugiat ad, magnam deserunt error commodi mollitia consequuntur hic laboriosam. Velit, dignissimos nulla eaque quis odio magni minus.</p>
+      <h1 className={styles.title}>Home Page</h1>
+      <p className={styles.text}>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Esse explicabo rem doloremque, fugiat ad, magnam deserunt error commodi mollitia consequuntur hic laboriosam. Velit, dignissimos nulla eaque quis odio magni minus.</p>
+      <p className={styles.text}>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Esse explicabo rem doloremque, fugiat ad, magnam deserunt error commodi mollitia consequuntur hic laboriosam. Velit, dignissimos nulla eaque quis odio magni minus.</p>
       
-      <Link href="/ninjas">
-        See Ninja Listing
-      </Link>
+      <Link href="/ninjas"><a  className={styles.btn}>See Ninja listing</a></Link>
     </div>
   )
 }
