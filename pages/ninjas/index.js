dc41ca7b20d8c3d01c53ea36c0685c989a3f79df
Abiona Michael <amabiona21@gmail.com>
2021-02-03 10:53:15 +0100

    feching data, using getStaticProps

--- pages/ninjas/index.js
+++ pages/ninjas/index.js
@@ -1,6 +1,16 @@
 import Head from "next/head";
+import styles from '../../styles/Ninjas.module.css';
 
-const Ninjas = () => {
+export const getStaticProps = async () => {
+  const res = await fetch('https://jsonplaceholder.typicode.com/users');
+  const ninjas = await res.json();
+
+  return {
+    props: { ninjas }
+  }
+}
+
+const Ninjas = ({ ninjas }) => {
   return (
     <>
       <Head>
@@ -9,6 +19,13 @@ const Ninjas = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <h1>Ninjas page</h1>
+      {ninjas.map(ninja => (
+        <div key={ninja.id}>
+          <a className={styles.single}>
+            <h3>{ ninja.name }</h3>
+          </a>
+        </div>
+      ))}
     </>
   );
 }
