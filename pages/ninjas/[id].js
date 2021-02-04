fcdec988814d0318cf7cfd11230b95e3542cd75a
Abiona Michael <amabiona21@gmail.com>
2021-02-04 01:44:55 +0100

    dynamic routing, using getStaticPaths

---
+++ pages/ninjas/[id].js
@@ -0,0 +1,31 @@
+
+export const getStaticProps = async ({ params }) => {
+  const res = await fetch(`https://jsonplaceholder.typicode.com/users/${params.id}`);
+  const ninja = await res.json();
+
+  return {
+    props: { ninja }
+  }
+}
+
+export const getStaticPaths = async () => {
+  const res = await fetch('https://jsonplaceholder.typicode.com/users');
+  const ninjas = await res.json();
+
+  const paths = ninjas.map(ninja => ({
+    params: { id: ninja.id }
+  }));
+
+  return {
+    paths,
+    fallback: false
+  }
+}
+
+const Details = ({ ninja }) => {
+  return (
+    <h1>{ninja.name} - {ninja.id}</h1>
+  );
+}
+ 
+export default Details;
--- pages/ninjas/test.js
+++
@@ -1,7 +0,0 @@
-const Test = () => {
-  return (
-    <h1>Test Page</h1>
-  );
-}
- 
-export default Test;
